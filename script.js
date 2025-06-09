3887c68696ffaafc45e54bb03eb5b5c687695d0b
mchaldebas <mchaldebas@rockefeller.edu>
2025-06-09 15:14:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 window.addEventListener('scroll', function() {
     const nav = document.getElementById('main-nav');
-    if (window.scrollY > window.innerHeight * 0.9) { // Appear after scrolling 90% of the hero section
+    if (window.scrollY > window.innerHeight * 0.85) {
         nav.classList.add('visible');
     } else {
         nav.classList.remove('visible');
     }
 });
+
+const FADE_IN_OBSERVER = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+const elementsToFadeIn = document.querySelectorAll('.fade-in');
+elementsToFadeIn.forEach((el) => FADE_IN_OBSERVER.observe(el));
